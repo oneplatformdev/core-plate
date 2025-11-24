eb99b21bfb76d89abe5fbf427e32fa6c1f130433
BohdanRadchenko <radchenkobs@gmail.com>
2025-11-24 13:08:36 +0200

    update external function to include importer parameter and validate input

--- external.ts
+++ external.ts
@@ -1,9 +1,13 @@
+import { isAbsolute } from 'node:path';
+
 const INTERNAL_ALIASES = [
 	'@op/',
 	'@oneplatformdev/plate/'
 ];
 
-export const external = (id: string) => {
+export const external = (id: string, importer?: string) => {
+	if (!importer) return false;
+	if (isAbsolute(id)) return false;
 	if (id.startsWith('.') || id.startsWith('/')) return false;
 	if (INTERNAL_ALIASES.some((p) => id.startsWith(p))) return false;
 	return true;
